5d89418b26c98acd06bb78ba12ca5286ad392fd2
Shaun Clancy <b00144642@mytudublin.ie>
2023-11-17 12:05:11 +0000

    vecel.com test

--- src/app/chart/page.js
+++ src/app/chart/page.js
@@ -14,6 +14,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import LinearProgress from '@mui/material/LinearProgress';
 import Alert from '@mui/material/Alert';
+import 'chart.js/auto';
 
 export default function Page() {
   const theme = createTheme({
