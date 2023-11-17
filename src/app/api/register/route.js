707db57dc040953e1d24298de1aef3091756f25a
Shaun Clancy <b00144642@mytudublin.ie>
2023-11-17 13:30:50 +0000

    Register test

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,42 +1,29 @@
-import { MongoClient } from 'mongodb';
-
 export async function GET(req, res) {
-  console.log("in the api page");
 
-  const { searchParams } = new URL(req.url);
-  const email = searchParams.get('email');
-  const numb = searchParams.get('numb');
-  const add = searchParams.get('add');
-  const pass = searchParams.get('pass');
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
+
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  const numb = searchParams.get('numb')
+  const add = searchParams.get('add')
+  const pass = searchParams.get('pass')
 
   console.log(email);
   console.log(pass);
   console.log(numb);
   console.log(add);
 
-  const uri = 'mongodb+srv://shaun:shaun123@cluster0.hgdl308.mongodb.net/?retryWrites=true&w=majority';
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  const dbName = 'app';
 
-  try {
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('login');
+ 
 
-    // Perform the database operation (insert, find, etc.)
-    const result = await collection.insertOne({ "username": email, "pass": pass });
-    console.log(`Inserted ${result.insertedCount} document into the collection`);
+  // database call goes here
 
-    // Send a response back
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ "data": "valid" }));
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-    res.writeHead(500, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ "error": "Internal Server Error" }));
-  } finally {
-    // Close the connection
-    await client.close();
-  }
+  // at the end of the process we need to send something back.
+  return Response.json({ "data":"valid" })
 }
+
