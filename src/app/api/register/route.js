f1c78d254717dedfc8fc1af6649cc5f207fdedd0
Shaun Clancy <b00144642@mytudublin.ie>
2023-11-24 14:35:57 +0000

    Login Test 3

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,19 +1,21 @@
 import { MongoClient } from 'mongodb';
-import { Response } from '@sveltejs/kit';
 
 // MongoDB Atlas connection URI
-const uri = 'mongodb+srv://shaun:shaun123@cluster0.hgdl308.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://shaun:<password>@cluster0.hgdl308.mongodb.net/?retryWrites=true&w=majority';
 
 // Database name
 const dbName = 'app';
 
-export async function POST(req, res) {
+export async function post(request) {
   try {
-    const { email, pass } = req.body;
+    const { email, pass } = request.body;
 
     // Validate input data
     if (!email || !pass) {
-      return Response.json({ error: 'Invalid data. Both email and password are required.' }, { status: 400 });
+      return {
+        status: 400,
+        body: { error: 'Invalid data. Both email and password are required.' },
+      };
     }
 
     // Connect to MongoDB Atlas
@@ -30,7 +32,10 @@ export async function POST(req, res) {
     const existingUser = await collection.findOne({ email });
 
     if (existingUser) {
-      return Response.json({ error: 'User with this email already exists.' }, { status: 400 });
+      return {
+        status: 400,
+        body: { error: 'User with this email already exists.' },
+      };
     }
 
     // Create a new user document
@@ -40,15 +45,24 @@ export async function POST(req, res) {
     };
 
     // Insert the new user into the 'login' collection
-    const result = await collection.insertOne(newUser);
+    await collection.insertOne(newUser);
 
     // Close the MongoDB connection
     await client.close();
 
     // Redirect to the login page upon successful registration
-    return Response.redirect(302, '/login'); // Adjust the path as needed
+    return {
+      status: 302,
+      headers: {
+        location: '/login', // Adjust the path as needed
+      },
+      body: {},
+    };
   } catch (error) {
     console.error('Error in API endpoint:', error);
-    return Response.json({ error: 'Internal Server Error' }, { status: 500 });
+    return {
+      status: 500,
+      body: { error: 'Internal Server Error' },
+    };
   }
 }
