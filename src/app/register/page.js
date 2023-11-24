9a478e1708b8a64d85347df62a354b00a8915b60
Shaun Clancy <b00144642@mytudublin.ie>
2023-11-24 14:30:06 +0000

    Login Test 2

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -24,7 +24,7 @@ export default function Register() {
 
     const userData = { email, pass };
 
-    const res = await fetch('http://localhost:3000/api/register', {
+    const res = await fetch('api/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
